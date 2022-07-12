fa7236812dc449d1bb94f333d8d23dfce0bb8bff
Pazzo97 <pazzomuk@gmail.com>
2022-07-12 14:38:16 +0200

    Created indexes

--- schema.sql
+++ schema.sql
@@ -44,3 +44,18 @@ CREATE TABLE visits (
 
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
+-- PERFOMANCE INCREASE AS PER TIME ON 'animals' TABLE
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4;
+
+CREATE INDEX animal_id_asc ON visits (animals_id ASC);
+
+SELECT * FROM visits where vets_id = 2;
+
+CREATE INDEX visits_id ON visits(date_of_visits ASC);
+
+EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
+
+-- PERFOMANCE INCREASE AS PER TIME ON 'owner' TABLE
+CREATE INDEX email_property ON owner(email ASC);
+
+EXPLAIN ANALYZE SELECT * FROM owner where email = 'owner_18327@mail.com';
